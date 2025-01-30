afb9e82c810d21f8b85398baa6900dce03b6c20e
naquinem <marlonagba@gmail.com>
2025-01-30 11:43:06 +0800

    Add routes for showing, updating, and deleting category

--- server/routes/api.php
+++ server/routes/api.php
@@ -6,23 +6,25 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Supplier\ProductController;
 use App\Http\Controllers\Supplier\CategoryController;
 
+//User registration
 Route::post('register', [AuthController::class, 'register']);
+//User sign-up
 Route::post('login', [AuthController::class, 'signin']);
+//Only authenticated users will access this routes
 Route::middleware('auth:api')->group(function(){
-    //categories
+    //categories CRUD
     Route::post('add-category', [CategoryController::class, 'store']);
     Route::get('show-categories', [CategoryController::class, 'index']);
-    //products
+    Route::get('show-category', [CategoryController::class, 'show']);
+    Route::put('update-category', [CategoryController::class, 'edit']);
+    Route::delete('delete-category', [CategoryController::class, 'destroy']);
+    //products CRUD
     Route::post('add-product', [ProductController::class, 'store']);
     Route::get('read-products', [ProductController::class, 'index']);
     Route::get('show-product', [ProductController::class, 'show']);
     Route::put('update-product', [ProductController::class, 'edit']);
-    Route::get('delete-product', [ProductController::class, 'destroy']);
+    Route::delete('delete-product', [ProductController::class, 'destroy']);
     //Logout for Authenticated user
     Route::post('logout', [AuthController::class, 'signout']);
 });
 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
